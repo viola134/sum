f8d5c77f310f81437cb2f6bbd39183a93b84c01e
viola134 <113242400+viola134@users.noreply.github.com>
2022-09-17 20:37:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,12 @@ $b = (rand(-10, 10));
 $c = (rand(-10, 10));
 $d = (rand(-10, 10));
 $x = $a*$b + $c*$d;
-echo("a=".$a); 
+echo("a=".$a." "); 
 
-echo("b=".$b);
+echo("b=".$b." ");
 
-echo("c=".$c);
+echo("c=".$c." ");
 
-echo("d=".$d);
+echo("d=".$d." ");
 
 echo("x=".$x);
