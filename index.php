651f7c773c4c24cd4c4555696ea31fcda2a5bff9
viola134 <violapakhomova13@gmail.com>
2022-09-17 20:16:28 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$a = (rand(-10, 10));
+$b = (rand(-10, 10));
+$c = (rand(-10, 10));
+$d = (rand(-10, 10));
+$x = $a*$b + $c*$d;
+echo("a=".$a); 
+
+echo("b=".$b);
+
+echo("c=".$c);
+
+echo("d=".$d);
+
+echo("x=".$x);
